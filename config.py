840e4039091c2c0890cb3df014375ae3f3023062
Khivrych Oleksandr <i.am.furious.from@gmail.com>
2020-06-30 16:27:11 +0300

    Create configuration for production

--- config.py
+++ config.py
@@ -3,10 +3,15 @@ import os
 
 logger = logging.getLogger(__name__)
 
-
-class DevelopmentConfig(object):
+class BaseConfiguration(object):
     BASE_DIR = os.path.dirname(os.path.realpath(__file__))
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{BASE_DIR}/db.sqlite"
+    SECRET_KEY = ""
+    ENCRYPTION_KEY = ""
+
+
+class DevelopmentConfig(BaseConfiguration):
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{BaseConfiguration.BASE_DIR}/db.sqlite"
 
     HOST = "localhost"
     PORT = "5000"
@@ -15,6 +20,13 @@ class DevelopmentConfig(object):
 
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    FLASK_ENV = 'development'
 
     # service code used for data blocking
     DATA_BLOCKING_CODE = "FTRS Data Blocking"
+
+
+class ProductionConfiguration(BaseConfiguration):
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FLASK_ENV = "production"
